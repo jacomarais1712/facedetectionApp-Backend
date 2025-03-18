4966406800a50c792ca3460d17efe6a86d09ea16
Jaco Marais <jaco@remotech.co.za>
2025-03-18 16:04:12 +0200

    Update server.js
    update server.js with process.env.PORT and process.env.DBPASS added to database variables

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const database = knex({
       host: '127.0.0.1',
       port: 5432,
       user: 'postgres',
-      password: '',
+      password: process.env.DBPASS,
       database: 'smart-brain',
     },
   });
@@ -32,6 +32,6 @@ app.get('/profile/:id', (req, res) => { profileid.handleProfile(req, res, databa
 app.put('/image', (req, res) => { image.handleImage(req, res, database) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-app.listen(3001, () => {
-    console.log('app is running at port 3001');
+app.listen(process.env.PORT, () => {
+    console.log(`app is running at port ${process.env.PORT}`);
 })
